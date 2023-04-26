c8f9d6bf3b7af6bb7b2ba50cca4b1a2d64a52fbd
Saba Kajaia <sabakajaia2@gmail.com>
2023-04-26 08:20:25 +0200

    1

---
+++ main.js
@@ -0,0 +1,10 @@
+// Estos correos ya se han apuntado a la notificación. Esto es para el ejercicio 2
+const registeredEmails = [
+  "paquita@gmail.com",
+  "rosa@gmail.com",
+  "faustino@gmail.com",
+];
+
+// PASO PREVIO: Añadir un listener al formulario para el event 'submit', y ejecutar el event.preventDefault()
+
+
